0595e71aaa1b5214c140b7e78f4f0d77bed09520
jakys <jakysing@163.com>
2023-07-12 05:20:56 +0000

    Change to master

--- api/withdraw/update.go
+++ api/withdraw/update.go
@@ -12,7 +12,7 @@ import (
 	"google.golang.org/grpc/status"
 )
 
-func (s *Server) UpdateWithdrawReview(ctx context.Context, in *npool.UpdateWithdrawReviewRequest) (*npool.UpdateWithdrawReviewResponse, error) { //nolint
+func (s *Server) UpdateWithdrawReview(ctx context.Context, in *npool.UpdateWithdrawReviewRequest) (*npool.UpdateWithdrawReviewResponse, error) {
 	handler, err := withdraw1.NewHandler(
 		ctx,
 		withdraw1.WithAppID(&in.AppID),
@@ -45,7 +45,7 @@ func (s *Server) UpdateWithdrawReview(ctx context.Context, in *npool.UpdateWithd
 	}, nil
 }
 
-func (s *Server) UpdateAppWithdrawReview(ctx context.Context, in *npool.UpdateAppWithdrawReviewRequest) (*npool.UpdateAppWithdrawReviewResponse, error) { //nolint
+func (s *Server) UpdateAppWithdrawReview(ctx context.Context, in *npool.UpdateAppWithdrawReviewRequest) (*npool.UpdateAppWithdrawReviewResponse, error) {
 	handler, err := withdraw1.NewHandler(
 		ctx,
 		withdraw1.WithAppID(&in.AppID),
