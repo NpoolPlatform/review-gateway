30d93ffb9b905af73f4adbc1be56cce4430fba8e
lidaqiang <dqli161@163.com>
2023-12-29 12:16:42 +0800

    change name

--- pkg/withdraw/coupon/query.go
+++ pkg/withdraw/coupon/query.go
@@ -37,7 +37,7 @@ func (h *queryHandler) getReviews(ctx context.Context) error {
 	}
 
 	infos, _, err := reviewmwcli.GetReviews(ctx, &reviewmwpb.Conds{
-		ObjectType: &basetypes.Uint32Val{Op: cruder.EQ, Value: uint32(reviewtypes.ReviewObjectType_ObjectCouponRandomCash)},
+		ObjectType: &basetypes.Uint32Val{Op: cruder.EQ, Value: uint32(reviewtypes.ReviewObjectType_ObjectRandomCouponCash)},
 		EntIDs:     &basetypes.StringSliceVal{Op: cruder.IN, Value: ids},
 	}, 0, int32(len(ids)))
 	if err != nil {
